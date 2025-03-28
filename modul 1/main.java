78841ab21203a37e2855053af3a55610d99c37df
kevin <daedara007gt@gmail.com>
2025-03-28 18:10:34 +0800

    pertamax

--- modul 1/main.java
+++ modul 1/main.java
@@ -2,17 +2,17 @@ class tabung {
     public double jari;
     public double tinggi; 
 
-    public tabung(double jari,double tinggi) {
+    public tabung(double jari, double tinggi) {
         this.jari = jari;
         this.tinggi = tinggi;
     }
     public static double hitungluas(double jari, double tinggi) {
-        return 2*Math.PI*jari*(jari+tinggi);
+        return 2 * Math.PI * jari * (jari + tinggi); 
     }
 }
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Luas tabungnya adalah "+new tabung(10,20));
+        System.out.println("Luas tabungnya adalah " + tabung.hitungluas(10, 20));
     }
 }
