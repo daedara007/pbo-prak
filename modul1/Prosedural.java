633bedb92f958cec08067950997a4106ada01238
kevin <daedara007gt@gmail.com>
2025-04-02 01:42:12 +0800

    final harusnya

--- modul1/Prosedural.java
+++ modul1/Prosedural.java
@@ -1,8 +1,13 @@
+// langsung class utama
 public class Prosedural {
     public static void main(String[] args) {
+        // ini adalah attribute untuk volume tabung
         double jari = 14;
         double tinggi = 1;
+
+        // dan bagian ini tanpa membuat objek langsung menghitung volume tabung
         double volume = Math.PI * Math.pow(jari, 2) * tinggi;
+        // output dari perhitungan di baris sebelumnya
         System.out.printf("Volume tabungnya adalah %.3f%n",
         volume);
     }
