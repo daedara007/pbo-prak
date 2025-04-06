38c922d7aa6476428bdbc677bcf432ef6deeb21b
unknown <kevin>
2025-04-06 23:18:56 +0800

    comment

--- modul4/OperasiMatriks.java
+++ modul4/OperasiMatriks.java
@@ -1,14 +1,19 @@
+// groupping modul4
 package modul4;
 
+// untuk import scanner
 import java.util.Scanner;
 
+// class utamanya
 public class OperasiMatriks {
     public static void main(String[] args) {
+        // objek scanner sebagai input matriks
         Scanner scanner = new Scanner(System.in);
+        // deklarasi matriksnya
         int[][] x = new int[2][2];
         int[][] y = new int[2][2];
         
-        // Input matriks X
+        // untuk input matriks X
         System.out.println("Masukkan elemen matriks X:");
         for(int i = 0; i < 2; i++) {
             for(int j = 0; j < 2; j++) {
@@ -17,7 +22,7 @@ public class OperasiMatriks {
             }
         }
         
-        // Input matriks Y
+        // untuk input matriks Y
         System.out.println("\nMasukkan elemen matriks Y:");
         for(int i = 0; i < 2; i++) {
             for(int j = 0; j < 2; j++) {
@@ -26,26 +31,32 @@ public class OperasiMatriks {
             }
         }
         
-        // Menu operasi
+        // Pilihan operasi matriks yanng mau dipilih
         System.out.println("\nPilih operasi:");
         System.out.println("1. Perkalian");
         System.out.println("2. Pengurangan");
         System.out.print("Masukkan pilihan (1/2): ");
         int choice = scanner.nextInt();
         
+        // switch case untuk pilihan sebelumnya
         switch(choice) {
+            // jika memilih perkalian
             case 1:
                 multiplyMatrices(x, y);
                 break;
+            // jika memilih pengurangan
             case 2:
                 subtractMatrices(x, y);
                 break;
+            // jika pilihan tidak valid
             default:
                 System.out.println("Pilihan tidak valid!");
         }
+        // objek scanner di close karena tidak digunakan lagi
         scanner.close();
     }
     
+    // method untuk perkalian matriks
     private static void multiplyMatrices(int[][] x, int[][] y) {
         int[][] result = new int[2][2];
         for(int i = 0; i < 2; i++) {
@@ -59,6 +70,7 @@ public class OperasiMatriks {
         printResult(result);
     }
     
+    // method untuk pengurangan matriks
     private static void subtractMatrices(int[][] x, int[][] y) {
         int[][] result = new int[2][2];
         for(int i = 0; i < 2; i++) {
@@ -69,6 +81,7 @@ public class OperasiMatriks {
         printResult(result);
     }
     
+    // method untuk menampilkan hasil operasi matriks
     private static void printResult(int[][] result) {
         System.out.println("\nHasil operasi:");
         for(int i = 0; i < 2; i++) {
