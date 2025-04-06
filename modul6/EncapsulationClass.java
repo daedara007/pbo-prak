9a63a491375aef4076909e4e6ccccc1d0b8ee18f
unknown <kevin>
2025-04-07 01:11:02 +0800

    rapihin

--- modul6/EncapsulationClass.java
+++ modul6/EncapsulationClass.java
@@ -33,79 +33,104 @@ class Biodata {
     }
 
     // Getter dan Setter untuk masing-masing atribut
+    // gettter untuk nama
     public String getNama() {
         return nama;
     }
+    // setter untuk nama
     public void setNama(String nama) {
         this.nama = nama;
     }
     
+    // getter untuk nim
     public String getNim() {
         return nim;
     }
+    // setter untuk nim
     public void setNim(String nim) {
         this.nim = nim;
     }
     
+    //getter untuk jurusan
     public String getJurusan() {
         return jurusan;
     }
+    //setter untuk jurusan
     public void setJurusan(String jurusan) {
         this.jurusan = jurusan;
     }
     
+    // getter untuk fakultas
     public String getFakultas() {
         return fakultas;
     }
+    // setter untuk fakultas
     public void setFakultas(String fakultas) {
         this.fakultas = fakultas;
     }
     
+
+    // getter untuk universitas
     public String getUniversitas() {
         return universitas;
     }
+    // setter untuk universitas
     public void setUniversitas(String universitas) {
         this.universitas = universitas;
     }
     
+
+    // getter untuk alamat
     public String getAlamat() {
         return alamat;
     }
+    // setter untuk alamat
     public void setAlamat(String alamat) {
         this.alamat = alamat;
     }
     
+    // getter untuk email
     public String getEmail() {
         return email;
     }
+    // setter untuk email
     public void setEmail(String email) {
         this.email = email;
     }
     
+    // getter untuk pekerjaan
     public String getPekerjaan() {
         return pekerjaan;
     }
+    // setter untuk pekerjaan
     public void setPekerjaan(String pekerjaan) {
         this.pekerjaan = pekerjaan;
     }
     
+    // getter untuk hobi
     public String getHobi() {
         return hobi;
     }
+    // setter untuk hobi
     public void setHobi(String hobi) {
         this.hobi = hobi;
     }
     
+    // getter untuk keahlian
     public String getKeahlian() {
         return keahlian;
     }
+    // setter untuk keahlian
     public void setKeahlian(String keahlian) {
         this.keahlian = keahlian;
     }
     
+
+    // getter untu karya
     public String getKarya() {
         return karya;
     }
+    // setter untuk karya
     public void setKarya(String karya) {
         this.karya = karya;
     }
